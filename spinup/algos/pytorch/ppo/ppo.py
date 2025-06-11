667fc80897f3810a0e8ee5af9580edd5f0766cb7
miznchimaki <miznchimaki@gmail.com>
2025-06-11 11:09:39 +0800

    nothing important..., a problem has been solved

--- spinup/algos/pytorch/ppo/ppo.py
+++ spinup/algos/pytorch/ppo/ppo.py
@@ -247,9 +247,6 @@ def ppo(
         obs, act, adv, logp_old = data['obs'], data['act'], data['adv'], data['logp']
 
         # Policy loss
-        # TODO: Now here
-        # TODO: 如果要是这么做的话,相当于每一个时间点的observation都有自己的normal distribution
-        # TODO: 这种做法合理么
         pi, logp = ac.pi(obs, act)
         ratio = torch.exp(logp - logp_old)
         clip_adv = torch.clamp(ratio, 1 - clip_ratio, 1 + clip_ratio) * adv
