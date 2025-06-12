faf7faf73498861b1c1c9a341895bfbb28346e89
miznchimaki <miznchimaki@gmail.com>
2025-06-12 10:15:58 +0800

    approximate KL-divergence is computed by Monte Carlo estimation

--- spinup/algos/pytorch/ppo/ppo.py
+++ spinup/algos/pytorch/ppo/ppo.py
@@ -253,8 +253,6 @@ def ppo(
         loss_pi = -(torch.min(ratio * adv, clip_adv)).mean()
 
         # Useful extra info
-        # TODO: Now here
-        # TODO: 仔细看一下KL Divergence的计算方式
         approx_kl = (logp_old - logp).mean().item()
         ent = pi.entropy().mean().item()
         clipped = ratio.gt(1 + clip_ratio) | ratio.lt(1 - clip_ratio)
@@ -338,11 +336,13 @@ def ppo(
 
             timeout = ep_len == max_ep_len
             terminal = d or timeout
-            epoch_ended = t==local_steps_per_epoch-1
+            epoch_ended = t == local_steps_per_epoch - 1
 
             if terminal or epoch_ended:
-                if epoch_ended and not(terminal):
+                if epoch_ended and not terminal:
                     print('Warning: trajectory cut off by epoch at %d steps.'%ep_len, flush=True)
+                # TODO: Now here
+                # TODO: 如果某个trajector没有到达terminal state,bootstrap value target的含义是什么
                 # if trajectory didn't reach terminal state, bootstrap value target
                 if timeout or epoch_ended:
                     _, v, _ = ac.step(torch.as_tensor(o, dtype=torch.float32))
@@ -355,7 +355,7 @@ def ppo(
                 o, ep_ret, ep_len = env.reset(), 0, 0
 
         # Save model
-        if (epoch % save_freq == 0) or (epoch == epochs-1):
+        if (epoch % save_freq == 0) or (epoch == epochs - 1):
             logger.save_state({'env': env}, None)
 
         # Perform PPO update!
